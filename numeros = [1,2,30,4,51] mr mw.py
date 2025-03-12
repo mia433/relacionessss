4eafc3d753a7f3f3586974476a0d9a8b4a3c1b2d
mia433 <wolfl.rios16@gmail.com>
2025-03-12 14:17:49 -0300

    Add files via upload

---
+++ numeros = [1,2,30,4,51] mr mw.py
@@ -0,0 +1,14 @@
+numeros = [1,2,30,4,51]
+for numero in numeros:
+    cuadrado = numero **2
+    print(f"el cuadrado de {numero} es {cuadrado}")
+
+print(numeros [4])
+
+
+nombre = "marcos"
+apellido = "mendoza"
+frace = "hola esta es una frase"
+
+longitud = len(frace)
+print(longitud)
